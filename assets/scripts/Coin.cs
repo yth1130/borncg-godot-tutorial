a8a6a2d0825d077ca8e18aca0232b67a96aa94bd
Taehoon Yoon <yooontehoon@naver.com>
2021-01-20 23:14:48 +0900

    8-1. Coin Scene, HUD, Spinnig Coin, Collision Detection

---
+++ assets/scripts/Coin.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public class Coin : Area
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GetNode<Timer>("Timer").Connect("timeout", this, nameof(OnTimeOut));
+        Connect("body_entered", this, nameof(OnBodyEntered));
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _PhysicsProcess(float delta)
+    {
+        RotateY(Mathf.Deg2Rad(3));
+    }
+
+    void OnBodyEntered(Node body)
+    {
+        if (body.Name == "Steve")
+        {
+            GD.Print("collect");
+            GetNode<Timer>("Timer").Start();
+        }
+    }
+
+    void OnTimeOut()
+    {
+        QueueFree();
+    }
+}
---
+++ assets/scripts/Counter.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class Counter : Label
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    int coins = 0;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
